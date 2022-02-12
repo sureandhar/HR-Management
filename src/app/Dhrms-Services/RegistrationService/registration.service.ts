dd7d17694a958aecaea8824aa6df39f9f74f30aa
sureandhar <www.sureandhar@gmail.com>
2022-02-12 19:23:38 +0530

    commit after reverting process change

--- src/app/Dhrms-Services/RegistrationService/registration.service.ts
+++ src/app/Dhrms-Services/RegistrationService/registration.service.ts
@@ -1,57 +1,57 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { from, Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { Iuser } from '../../dhrms-Interface/User';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { from, Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { Iuser } from "../../dhrms-Interface/User";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class RegistrationService {
   _url: any;
   constructor(private http: HttpClient, private _jwtHelper: JwtHelperService) {
-    this._url = process.env.prod_url || '';
+    //this._url = process.env.prod_url || '';
   }
 
   setUserdetails() {
-    var _rolename = '';
-    const token = localStorage.getItem('jwt');
+    var _rolename = "";
+    const token = localStorage.getItem("jwt");
     const decodedToken = this._jwtHelper.decodeToken(token);
-    const isRemember = localStorage.getItem('isrembember');
-    if (isRemember == 'true') {
-      console.log('inside local');
+    const isRemember = localStorage.getItem("isrembember");
+    if (isRemember == "true") {
+      console.log("inside local");
       if (token && !this._jwtHelper.isTokenExpired(token)) {
-        console.log('setting token1');
+        console.log("setting token1");
         //localStorage.setItem('jwt',token);
-        localStorage.setItem('email', btoa(decodedToken.email));
-        localStorage.setItem('role', btoa(decodedToken.role));
+        localStorage.setItem("email", btoa(decodedToken.email));
+        localStorage.setItem("role", btoa(decodedToken.role));
         _rolename = decodedToken.role;
       }
     } else {
-      console.log('setting session1');
-      sessionStorage.setItem('email', btoa(decodedToken.email));
-      sessionStorage.setItem('role', btoa(decodedToken.role));
+      console.log("setting session1");
+      sessionStorage.setItem("email", btoa(decodedToken.email));
+      sessionStorage.setItem("role", btoa(decodedToken.role));
       _rolename = decodedToken.role;
     }
     return _rolename;
   }
 
   logout() {
-    localStorage.removeItem('jwt');
-    localStorage.removeItem('email');
-    localStorage.removeItem('role');
-    localStorage.removeItem('isrembember');
-    sessionStorage.removeItem('email');
-    sessionStorage.removeItem('role');
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("email");
+    localStorage.removeItem("role");
+    localStorage.removeItem("isrembember");
+    sessionStorage.removeItem("email");
+    sessionStorage.removeItem("role");
   }
   getUserdetails(key: string) {
-    var _rolename = '';
-    var _email = '';
-    var _return = '';
-    const token = localStorage.getItem('jwt');
+    var _rolename = "";
+    var _email = "";
+    var _return = "";
+    const token = localStorage.getItem("jwt");
     const decodedToken = this._jwtHelper.decodeToken(token);
-    const isRemember = localStorage.getItem('isrembember');
+    const isRemember = localStorage.getItem("isrembember");
     if (isRemember) {
       if (token && !this._jwtHelper.isTokenExpired(token)) {
         //localStorage.setItem('jwt',token);
@@ -67,10 +67,10 @@ export class RegistrationService {
       _email = decodedToken.email;
     }
     switch (key.toLocaleLowerCase()) {
-      case 'email':
+      case "email":
         _return = _email;
         break;
-      case 'role':
+      case "role":
         _return = _rolename;
         break;
 
@@ -86,7 +86,7 @@ export class RegistrationService {
     console.log(userObj);
     return this.http
       .post<string>(
-        'https://localhost:44358/api/dhrms/ValidateUserCredntials',
+        "https://localhost:44358/api/dhrms/ValidateUserCredntials",
         userObj
       )
       .pipe(catchError(this.errorHandler));
@@ -99,7 +99,7 @@ export class RegistrationService {
     userObj = { Email: _email, Userpassword: _password };
     console.log(userObj);
     return this.http
-      .post<string>('https://localhost:44358/api/auth/login', userObj)
+      .post<string>("https://localhost:44358/api/auth/login", userObj)
       .pipe(catchError(this.errorHandler));
   }
 
@@ -113,10 +113,10 @@ export class RegistrationService {
       TempPassword: _temppass,
       Password: _pass,
     };
-    const headers = { 'content-type': 'application/json' };
+    const headers = { "content-type": "application/json" };
     const body = JSON.stringify(params);
     return this.http
-      .post<string>('https://localhost:44358/api/dhrms/Updatepassword', body, {
+      .post<string>("https://localhost:44358/api/dhrms/Updatepassword", body, {
         headers: headers,
       })
       .pipe(catchError(this.errorHandler));
